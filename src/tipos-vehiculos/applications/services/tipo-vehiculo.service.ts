2d288c7c8cc45b056efd30f4de96a448bb85a0b8
Thetander <joelzunta@hotmail.com>
2024-11-13 16:07:30 -0500

    creacion de servicio y controller tipo-vehiculo

--- src/tipos-vehiculos/applications/services/tipo-vehiculo.service.ts
+++ src/tipos-vehiculos/applications/services/tipo-vehiculo.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TipoVehiculoEntity } from '../../domain/entities/tipos-vehiculos.entity';
+import { CreateTipoVehiculoDto } from '../dto/create-tipo-vehiculo.dto';
+import { EditTipoVehiculoDto } from '../dto/update-tipo-vehiculo.dto';
 
 @Injectable()
-export class TipoVehiculoService {}
+export class TipoVehiculoService {
+    constructor(
+        @InjectRepository(TipoVehiculoEntity)
+        private readonly tipoVehiculoRepository: Repository<TipoVehiculoEntity>,
+    ) {}
+
+    async getMany() {
+        return await this.tipoVehiculoRepository.find();
+    }
+
+    async getOne(idTipoVehiculo: number) {
+        const tipoVehiculo = await this.tipoVehiculoRepository.findOne({ where: { idTipoVehiculo } });
+        if (!tipoVehiculo) throw new NotFoundException('Tipo de Vehiculo not found');
+        return tipoVehiculo;
+    }
+
+    async createOne(dto: CreateTipoVehiculoDto) {
+        const tipoVehiculo = this.tipoVehiculoRepository.create(dto);
+        return await this.tipoVehiculoRepository.save(tipoVehiculo);
+    }
+
+    async editOne(idTipoVehiculo: number, dto: EditTipoVehiculoDto) {
+        const tipoVehiculo = await this.getOne(idTipoVehiculo);
+        const editedTipoVehiculo = Object.assign(tipoVehiculo, dto);
+        return await this.tipoVehiculoRepository.save(editedTipoVehiculo);
+    }
+
+    async deleteOne(idTipoVehiculo: number) {
+        const tipoVehiculo = await this.getOne(idTipoVehiculo);
+        return await this.tipoVehiculoRepository.remove(tipoVehiculo);
+    }
+}
--- src/tipos-vehiculos/infrastructure/controllers/tipo-vehiculo.controller.ts
+++ src/tipos-vehiculos/infrastructure/controllers/tipo-vehiculo.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { TipoVehiculoService } from '../../applications/services/tipo-vehiculo.service';
+import { CreateTipoVehiculoDto } from '../../applications/dto/create-tipo-vehiculo.dto';
+import { EditTipoVehiculoDto } from '../../applications/dto/update-tipo-vehiculo.dto';
 
-@Controller('tipo-vehiculo')
-export class TipoVehiculoController {}
+@ApiTags('tipos-vehiculos')
+@Controller('tipos-vehiculos')
+export class TipoVehiculoController {
+    constructor(private readonly tipoVehiculoService: TipoVehiculoService) {}
+
+    @Get()
+    async getMany() {
+        const data = await this.tipoVehiculoService.getMany();
+        return { data };
+    }
+
+    @Get(':id')
+    async getOne(@Param('id') id: number) {
+        const data = await this.tipoVehiculoService.getOne(id);
+        return { data };
+    }
+
+    @Post()
+    async createOne(@Body() dto: CreateTipoVehiculoDto) {
+        const data = await this.tipoVehiculoService.createOne(dto);
+        return { message: 'Tipo de Vehiculo created', data };
+    }
+
+    @Put(':id')
+    async editOne(@Param('id') id: number, @Body() dto: EditTipoVehiculoDto) {
+        const data = await this.tipoVehiculoService.editOne(id, dto);
+        return { message: 'Tipo de Vehiculo updated', data };
+    }
+
+    @Delete(':id')
+    async deleteOne(@Param('id') id: number) {
+        const data = await this.tipoVehiculoService.deleteOne(id);
+        return { message: 'Tipo de Vehiculo deleted', data };
+    }
+}
