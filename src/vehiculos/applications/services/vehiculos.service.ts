794264eaf533fc1865e0d447c2896c101b18af6c
Thetander <joelzunta@hotmail.com>
2024-11-28 15:00:48 -0500

    corrigiendo entidad. agrego campos, controll color marca modelo

--- src/vehiculos/applications/services/vehiculos.service.ts
+++ src/vehiculos/applications/services/vehiculos.service.ts
@@ -4,6 +4,10 @@ import { DeepPartial, Repository } from 'typeorm';
 import { VehiculoEntity } from 'src/vehiculos/domain/entities/vehiculos.entity/vehiculos.entity';
 import { CreateVehiculoDto } from '../dto/create-vehiculos.dto';
 import { EditVehiculoDto } from '../dto/update-vehiculos.dto';
+import { ColorEntity } from 'color/domain/entities/colores.entity';
+import { MarcaEntity } from 'marca/domain/entities/marca.entity';
+import { ModeloEntity } from 'modelo/domain/entities/modelo.entity';
+import { TipoVehiculoEntity } from 'tipos-vehiculos/domain/entities/tipos-vehiculos.entity';
 
 @Injectable()
 export class VehiculoService {
@@ -47,9 +51,37 @@ export class VehiculoService {
     }
 
     async createOne(dto: CreateVehiculoDto) {
-        const newVehiculo = this.vehiculoRepository.create(dto as  DeepPartial<VehiculoEntity>);
+        const color = await this.vehiculoRepository.manager.findOne(ColorEntity, { where: { idColor: dto.idColor } });
+        if (!color) {
+            throw new NotFoundException(`Color con id ${dto.idColor} no encontrado`);
+        }
+    
+        const marca = await this.vehiculoRepository.manager.findOne(MarcaEntity, { where: { idMarca: dto.idMarca } });
+        if (!marca) {
+            throw new NotFoundException(`Marca con id ${dto.idMarca} no encontrada`);
+        }
+    
+        const modelo = await this.vehiculoRepository.manager.findOne(ModeloEntity, { where: { idModelo: dto.idModelo } });
+        if (!modelo) {
+            throw new NotFoundException(`Modelo con id ${dto.idModelo} no encontrado`);
+        }
+    
+        const tipoVehiculo = await this.vehiculoRepository.manager.findOne(TipoVehiculoEntity, { where: { idTipoVehiculo: dto.idTipoVehiculo } });
+        if (!tipoVehiculo) {
+            throw new NotFoundException(`Tipo de Vehículo con id ${dto.idTipoVehiculo} no encontrado`);
+        }
+    
+        const newVehiculo = this.vehiculoRepository.create({
+            ...dto,
+            color,
+            marca,
+            modelo,
+            tipoVehiculo,
+        });
+    
         return await this.vehiculoRepository.save(newVehiculo);
     }
+    
 
     async editOne(id: number, dto: EditVehiculoDto) {
         const vehiculo = await this.getOne(id);
