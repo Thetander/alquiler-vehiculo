31a70522c02273163abd8d0b7400439ec10d82f4
Thetander <joelzunta@hotmail.com>
2025-01-12 17:07:19 -0500

    ajuste a empleados

--- src/empleados/applications/modules/empleados.module.ts
+++ src/empleados/applications/modules/empleados.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { EmpleadosController } from '../../infrastructure/controllers/empleados.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmpleadoEntity } from 'src/empleados/domain/entities/empleados.entity';
+import { PersonasModule } from 'personas/applications/modules/personas.module';
+import { EmpleadosService } from '../services/empleados.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EmpleadoEntity])],
-  controllers: [EmpleadosController]
+  imports: [TypeOrmModule.forFeature([EmpleadoEntity]),
+PersonasModule],
+  
+  controllers: [EmpleadosController],
+  providers:[EmpleadosService],
+  exports:[EmpleadosService ]
+
 })
 export class EmpleadosModule {}
