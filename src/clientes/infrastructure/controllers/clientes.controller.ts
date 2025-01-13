f8939aa9bd2e4c5d757f1f9b9925d1ad53d60b22
Thetander <joelzunta@hotmail.com>
2025-01-13 16:15:13 -0500

    correcion

--- src/clientes/infrastructure/controllers/clientes.controller.ts
+++ src/clientes/infrastructure/controllers/clientes.controller.ts
@@ -15,10 +15,6 @@ export class ClienteController {
         return { data };
     }
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> 55048c13cf0472e818fd8cf1dde3fcc7f04a3606
 
     @Get(':id')
     async getOne(@Param('id') id: number) {
